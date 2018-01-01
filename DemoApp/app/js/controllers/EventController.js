4e055549515942f8484c8092379e3968acf93405
txrackack <34013498+txrackack@users.noreply.github.com>
2018-01-01 14:37:20 -0600

    initial add of project
    working initial base project

---
+++ DemoApp/app/js/controllers/EventController.js
@@ -0,0 +1,11 @@
+'use strict';
+
+eventsApp.controller('EventController',
+    function EventController($scope){
+        $scope.event={
+          name: 'Angular Boot Camp',
+          date: '1/1/2013',
+          time: '10:30 am'
+        }
+    }
+);
