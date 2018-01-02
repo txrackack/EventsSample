bb012c8ba54c26f361e34e15fdd472b9e148903f
txrackack <34013498+txrackack@users.noreply.github.com>
2018-01-01 22:07:35 -0600

    Finished Controllers and Markup
    fully functional as of the end of the Angular Controllers & Markup section

--- DemoApp/app/js/controllers/EventController.js
+++ DemoApp/app/js/controllers/EventController.js
@@ -2,6 +2,7 @@
 
 eventsApp.controller('EventController',
     function EventController($scope){
+        $scope.sortorder = 'name';
         $scope.event={
             name: 'Angular Boot Camp',
             date: '1/1/2013',
@@ -16,7 +17,7 @@ eventsApp.controller('EventController',
                 {
                     name: 'Directives Masterclass',
                     creatorName: 'Bob Smith',
-                    duration: '1 hr',
+                    duration: 1,
                     level: 'Advanced',
                     abstract: 'In this session you will learn the ins and outs of directives!',
                     upVoteCount: 0
@@ -24,7 +25,7 @@ eventsApp.controller('EventController',
                 {
                     name: 'Scopes for fun and profit',
                     creatorName: 'John Doe',
-                    duration: '30 mins',
+                    duration: 2,
                     level: 'Introductory',
                     abstract: 'This session will take a closer look at scopes.  Learn what they do, how they do it, and how to get them to do it for you.',
                     upVoteCount: 0
@@ -32,7 +33,7 @@ eventsApp.controller('EventController',
                 {
                     name: 'Well Behaved Controllers',
                     creatorName: 'Jane Doe',
-                    duration: '2 hours',
+                    duration: 4,
                     level: 'Intermediate',
                     abstract: 'Controllers are the beginning of everything Angular does.  Learn how to craft controllers that will win the respect of your friends and neighbors.',
                     upVoteCount: 0
